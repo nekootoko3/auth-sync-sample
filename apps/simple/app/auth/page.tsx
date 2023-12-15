c93535bc0e7ba55412e274d857f503873b86114f
nekootoko3 <miyako.kerria@gmail.com>
2023-12-15 16:58:02 +0900

    シンプルなケースの実装

---
+++ apps/simple/app/auth/page.tsx
@@ -0,0 +1,40 @@
+"use client";
+
+export default function Page(): JSX.Element {
+  const logInToTenantA = () => {
+    localStorage.setItem("tenantName", "tenant-a");
+  };
+  const logInToTenantB = () => {
+    localStorage.setItem("tenantName", "tenant-b");
+  };
+  const logInToTenantC = () => {
+    localStorage.setItem("tenantName", "tenant-b");
+  };
+  const logOut = () => {
+    localStorage.removeItem("tenantName");
+  };
+
+  return (
+    <main>
+      <h3>認証</h3>
+
+      <div style={{ display: "flex", flexDirection: "column", gap: "4px" }}>
+        <div>
+          <button onClick={logInToTenantA}>テナントAにログイン</button>
+        </div>
+
+        <div>
+          <button onClick={logInToTenantB}>テナントBにログイン</button>
+        </div>
+
+        <div>
+          <button onClick={logInToTenantC}>テナントCにログイン</button>
+        </div>
+
+        <div>
+          <button onClick={logOut}>ログアウト</button>
+        </div>
+      </div>
+    </main>
+  );
+}
---
+++ apps/simple/app/dashboard/page.tsx
@@ -0,0 +1,39 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
+export default function Page(): JSX.Element {
+  const [tenantName, setTenantName] = useState<string | null | undefined>(
+    undefined
+  );
+
+  useEffect(() => {
+    setTenantName(localStorage.getItem("tenantName"));
+  }, []);
+
+  useEffect(() => {
+    window.addEventListener("storage", listenStorageAuthEvent);
+
+    return () => {
+      window.removeEventListener("storage", listenStorageAuthEvent);
+    };
+  }, []);
+
+  if (tenantName === undefined) return <p>読み込み中...</p>;
+
+  return (
+    <main>
+      {tenantName ? (
+        <p>{tenantName}にログインしています。</p>
+      ) : (
+        <p>ログインしてください</p>
+      )}
+    </main>
+  );
+}
+
+const listenStorageAuthEvent = (event: StorageEvent) => {
+  if (event.key !== "tenantName") return;
+
+  window.location.reload();
+};
